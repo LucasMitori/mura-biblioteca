04f0227c676173f46e1c370b67b0558af911d1c3
Lucas Mitori <103422552+LucasMitori@users.noreply.github.com>
2023-04-02 12:47:13 -0300

    v.0.51

--- src/components/PaymentCheckout/PaymentCheckout.jsx
+++ src/components/PaymentCheckout/PaymentCheckout.jsx
@@ -39,13 +39,19 @@ const PaymentCheckout = () => {
 
   const formSchema = yup.object().shape({
     email: yup.string().required("E-mail obrigatório").email("E-mail inválido"),
-    password: yup
+    cardDetails: yup
       .string()
-      .matches(
-        /(^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*_-])).{8,}$/,
-        "Deve conter 8 caraceteres, uma maiúscula, uma minúscula, um número e um caracter especial"
-      )
-      .required("escolha uma senha"),
+      .required("Detalhes do cartão obrigatório")
+      .matches(/^4[0-9]{15}$/, "Deve conter os números do cartão"),
+    cardDetailsDate: yup
+      .string()
+      .required("Detalhes do cartão obrigatório")
+      .matches(/^(0[1-9]|1[0-2])\/[0-9]{2}$/, "Data incorreta"),
+    cardDetailsCVV: yup.string().required("Detalhes do cartão obrigatório"),
+    cardHolder: yup.string().required("nome do portador obrigatório"),
+    AddressCountry: yup.string().required("Endereço obrigatório"),
+    AddressZip: yup.string().required("Endereço obrigatório"),
+    AddressState: yup.string().required("Endereço obrigatório"),
   });
 
   const {
@@ -55,6 +61,10 @@ const PaymentCheckout = () => {
   } = useForm({
     resolver: yupResolver(formSchema),
   });
+
+  const test = (data) => {
+    console.log(data);
+  };
 
   return (
     <>
@@ -104,10 +114,15 @@ const PaymentCheckout = () => {
               <h1>Payment Details</h1>
               <p>Complete your purchase by providing your payment details</p>
             </PaymentDetails>
-            <FormSpace onSubmit={handleSubmit()}>
+            <FormSpace onSubmit={handleSubmit(test)}>
               <EmailSpace>
                 <h2>Email address</h2>
-                <input type="email" />
+                <input
+                  type="email"
+                  id="email"
+                  placeholder="Seu e-mail"
+                  {...register("email")}
+                />
               </EmailSpace>
               <CardDetails>
                 <h2>Card details</h2>
@@ -120,34 +135,63 @@ const PaymentCheckout = () => {
                   >
                     <FaCcVisa />
                   </IconContext.Provider>
-                  <input type="text" placeholder="Card Details" />
+                  <input
+                    type="text"
+                    placeholder="Card Details"
+                    id="cardDetails"
+                    {...register("cardDetails")}
+                  />
                   <CardDetailsInputDates>
-                    <input type="text" placeholder="MM/YY" />
-                    <input type="password" placeholder="CVV" />
+                    <input
+                      type="text"
+                      placeholder="MM/YY"
+                      id="cardDetailsDate"
+                      {...register("cardDetailsDate")}
+                    />
+                    <input
+                      type="password"
+                      placeholder="CVV"
+                      id="cardDetailsCVV"
+                      {...register("cardDetailsCVV")}
+                    />
                   </CardDetailsInputDates>
                 </CardDetailsInputDiv>
                 <CardHolderName>
                   <h2>Cardholder name</h2>
-                  <input type="text" />
+                  <input
+                    type="text"
+                    id="cardHolder"
+                    {...register("cardHolder")}
+                  />
                 </CardHolderName>
                 <BillingAddress>
                   <h2>Billing address</h2>
                   <AddressDiv>
-                    <select>
+                    <select id="AddressCountry" {...register("AddressCountry")}>
                       <option hidden>Brasil</option>
                       <option>India</option>
                       <option>Australia</option>
                       <option>Canada</option>
                     </select>
                     <StateDiv>
-                      <input type="text" placeholder="ZIP" />
-                      <input type="text" placeholder="State" />
+                      <input
+                        type="text"
+                        placeholder="ZIP"
+                        id="AddressZip"
+                        {...register("AddressZip")}
+                      />
+                      <input
+                        type="text"
+                        placeholder="State"
+                        id="AddressState"
+                        {...register("AddressState")}
+                      />
                     </StateDiv>
                   </AddressDiv>
                   <VATNumber>
                     <h2>VAT Number</h2>
                     <VATCheck>
-                      <input type="text" />
+                      <input type="text" id="VAT" {...register("VAT")} />
                       <IconContext.Provider
                         value={{
                           color: "var(--color-black-mode)",
@@ -180,9 +224,9 @@ const PaymentCheckout = () => {
                       </p>
                     </div>
                   </Subtotal>
-                  <PayButton>Pay</PayButton>
                 </BillingAddress>
               </CardDetails>
+              <PayButton type="submit">Pay</PayButton>
             </FormSpace>
           </BoxInner2>
         </SecondBox>
